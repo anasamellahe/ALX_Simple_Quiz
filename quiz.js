71fd516594562d700ef43387a29a4e13b736da75
anamella <amellaheanas@gmail.com>
2025-06-15 18:50:34 +0100

    1. Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,18 @@
+
+function checkAnswer()
+{
+    const feedback = document.getElementById("feedback");
+    const correctAnswer = "4";
+    const userAnswer =  document.querySelector('input[name="quiz"]:checked');
+    if (!userAnswer) {
+        feedback.textContent = "You must select an answer.";
+        return;
+    }
+    if (userAnswer.value == correctAnswer)
+        feedback.textContent  = "Correct! Well done.";
+    else
+        feedback.textContent  = "That's incorrect. Try again!";
+}
+
+const submitAnswer =  document.getElementById("submit-answer");
+submitAnswer.addEventListener('click', checkAnswer);
